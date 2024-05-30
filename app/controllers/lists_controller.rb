33bd6affc1e4929a7c784334f681f65042e5fe82
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-05-30 18:05:10 -0300

    Não terminei

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    if list.save
+      redirect_to lists_path, notice: 'List was succefully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to @list, notice: 'List was succefully updated!',status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
 
+  #  resources :movies, only: %i[new create]
+
+  # resources :bookmarks, only: %i[new create index show destroy]
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: %i[destroy]
 end
